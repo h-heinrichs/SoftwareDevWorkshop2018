7ef81c5bc37d7dd7a3daf0411ef75c08873e0faa
Heidi Heinrichs <h.heinrichs@iwv3.kfa-juelich.de>
2018-05-30 12:08:54 +0200

    new1

--- printNumbers/functions/factorial.py
+++ printNumbers/functions/factorial.py
@@ -18,6 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with printNumbers.  If not, see <http://www.gnu.org/licenses/>.
+# some changes made
 
 import math
 
